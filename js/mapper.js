5390fed613d8ee664b5e7c966032c23edc4126e5
Mia Averse <33697459+miaflatpaddy@users.noreply.github.com>
2024-04-30 16:10:58 +0300

    Update mapper.js

--- js/mapper.js
+++ js/mapper.js
@@ -4,18 +4,16 @@
         x.forEach(element => {
             let u = element;
             array[i]=new Map();
-            console.log(rules);
             rules.forEach(element1 => {
                 let k = element1;
                 array[i].set(element1[1],u[element1[0]])
-                console.log(element1[2]);
                 if(typeof element1[2]!== "undefined"){
-                    array[i].set(k[1],element1[2](array[i][k[1]]))
-
+                    array[i].set(k[1],element1[2](u[k[0]]));
                 }
             });
             i++;
         });
+        return array;
     }
 let testData3 = [{"name":"Vasya","email":"vasya@example.com","age":20,"skills":{"php":0,"js":-1,"madness":10,"rage":10}},{"name":"Dima","email":"dima@example.com","age":34,"skills":{"php":5,"js":7,"madness":3,"rage":2}},{"name":"Colya","email":"colya@example.com","age":46,"skills":{"php":8,"js":-2,"madness":1,"rage":4}},{"name":"Misha","email":"misha@example.com","age":16,"skills":{"php":6,"js":6,"madness":5,"rage":2}},{"name":"Ashan","email":"ashan@example.com","age":99,"skills":{"php":0,"js":10,"madness":10,"rage":1}},{"name":"Rafshan","email":"rafshan@example.com","age":11,"skills":{"php":0,"js":0,"madness":0,"rage":10}}]
 
@@ -24,4 +22,4 @@ const mapRules = [
   ["age", "a"]
 ]
 
-console.log(mapper(testData3,mapRules)) // [{"n":"vasya","a":20},{"n":"dima","a":34},{"n":"colya","a":46},{"n":"misha","a":16},{"n":"ashan","a":99},{"n":"rafshan","a":11}]
+console.log(mapper(testData3,mapRules)) 
