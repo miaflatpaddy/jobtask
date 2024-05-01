041c63db498b80c985f57dbc0933bd90bccd9a96
Mia Averse <33697459+miaflatpaddy@users.noreply.github.com>
2024-05-01 16:57:20 +0300

    Add files via upload

---
+++ react/1-Modal/Modal.jsx
@@ -0,0 +1,17 @@
+﻿import React from "react";
+import "./modal.css";
+const Modal = ({active,setActive,children,disableGlobalScroll}) => {
+    if(disableGlobalScroll&& active){
+        console.log("noscroll");
+        document.body.style.overflow='hidden';
+    }
+    return (
+        <div className = {active ? "modal active" : "modal"} onClick={() => [setActive(false),document.body.style.overflow='visible']}>
+            <div className={active ? "modal__content active" : "modal__content"} onClick={e=>e.stopPropagation}>
+                {children}
+            </div>
+        </div>
+    );
+};
+export default Modal;
+
