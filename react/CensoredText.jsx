d334f7a4e26080de0600fda43a281d1513367b2e
Mia Averse <33697459+miaflatpaddy@users.noreply.github.com>
2024-05-01 17:43:48 +0300

    Add files via upload

---
+++ react/CensoredText.jsx
@@ -0,0 +1,23 @@
+﻿import React from "react";
+const CensoredText = ({badWords,children}) => {
+    let str = children;
+    let placeholder='';
+    let sstr = str.split(/\b/);
+    badWords.forEach(element => {
+        for (let i = 0; i < element.length; i++) {
+            placeholder+='*';   
+        }
+        for (let i = 0; i < sstr.length; i++) {
+            if(sstr[i]===element){
+                sstr[i] = placeholder;
+            }
+        }
+    });
+    str = sstr.join('');
+    return (
+        <div className = "censoredWords">
+            {str}
+        </div>
+    );
+};
+export default CensoredText;
